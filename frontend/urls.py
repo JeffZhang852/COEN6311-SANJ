3f33486f7a8a38af5285fa19fe13fc0bfc5c636e
Ammar Al Zib <ammaralzib@hotmail.com>
2026-02-22 16:49:11 -0500

    more frontend

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,12 +3,21 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+
+    path ("memberships/", views.memberships, name='memberships'),
+    path ("nutrition/", views.nutrition, name='nutrition'),
+
+
+    path ("amenities/", views.amenities, name='amenities'),
+    #path ("memberships/", views.memberships, name='memberships'),
+
+
+
+
+
     path('gym/', views.gym, name='gym'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('signedout/', views.signed_out, name='signed_out'),
-
-    path ("memberships/", views.memberships, name='memberships')
-
-
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -2,20 +2,44 @@ from django.shortcuts import render
 
 
 
-# Create your views here.
+#Navbar
 def home(request):
     return render(request, 'frontend/home.html')
-
-def test(request):
-    return render(request, 'test.html')
-
-def gym(request):
-    return render(request, 'frontend/gym.html')
 
 
 def memberships(request):
     return render(request, 'frontend/membership.html')
 
+def trainers(request):
+    return render(request, 'frontend/trainers.html')
+
+def nutrition(request):
+    return render(request, 'frontend/nutrition.html')
+
+
+#Dropdown menu
+def amenities(request):
+    return render(request, 'frontend/amenities.html')
+
+def schedule(request):
+    return render(request, 'frontend/schedule.html')
+
+def contact(request):
+    return render(request, 'frontend/contact.html')
+def about(request):
+    return render(request, 'frontend/about.html')
+
+
+
+
+
+def gym(request):
+    return render(request, 'frontend/gym.html')
+
+
+
+
+#Registration
 def login(request):
     return render(request, 'authentication/login.html')
 
